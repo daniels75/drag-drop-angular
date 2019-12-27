05b7ff6dcb070e19577a23342204aa1ff74c0ef3
daniels75 <daniel.sadowski75@gmail.com>
2019-12-27 16:42:57 +0100

    Added content of the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'drag-drop-angular';
+  title = 'Drag & Drop in Angular 8';
+
+  todos = [
+    {
+      name: 'Angular',
+      category: 'Web Development'
+    },
+    {
+      name: 'Flexbox',
+      category: 'Web Development'
+    },
+    {
+      name: 'iOS',
+      category: 'App Development'
+    },
+    {
+      name: 'Java',
+      category: 'Software development'
+    }
+  ];
+
+  completed = [
+    {
+      name: 'Android',
+      category: 'Mobile Development'
+    },
+    {
+      name: 'MongoDB',
+      category: 'Databases'
+    },
+    {
+      name: 'ARKit',
+      category: 'Augmented Reality'
+    },
+    {
+      name: 'React',
+      category: 'Web Development'
+    }
+  ];
 }
